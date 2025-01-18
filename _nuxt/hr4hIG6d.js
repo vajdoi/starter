ceb3b69f3a0d1d002909f0783969d2b2359c8073
vajdoi <168217348+vajdoi@users.noreply.github.com>
2025-01-18 14:53:04 +0000

    Deploying to gh-pages from @ vajdoi/starter@a4ebc32e0c470780c5f3253c9a9c7e0fb554cb43 🚀

---
+++ _nuxt/hr4hIG6d.js
@@ -0,0 +1,1 @@
+import{l as r,k as n,f as o,V as s}from"./B4N5WGM9.js";const t={};function a(e,c){return n(),o("strong",null,[s(e.$slots,"default")])}const _=Object.assign(r(t,[["render",a]]),{__name:"ProseStrong"});export{_ as default};
